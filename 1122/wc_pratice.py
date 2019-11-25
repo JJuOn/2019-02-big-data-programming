027d2e44fc61cd6d005f88f7cb2ae78270b3f0a0
JJuOn <jwseo001@khu.ac.kr>
2019-11-26 04:54:13 +0900

    Modify wc_practice.py to use HDFS

--- 1122/wc_pratice.py
+++ 1122/wc_pratice.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
 		print >> sys.stderr, "Usage: wordcount <master> <inputfile> <outputfile>"
 		exit(-1)
 	sc = SparkContext(sys.argv[1], "python_wordcount_sorted in bigdataprogramming")
-	lines = sc.textFile(sys.argv[2],2)
+	lines = sc.textFile('hdfs://localhost:9000/user/hadoop/'+sys.argv[2],2)
 	print(lines.getNumPartitions()) # print the number of partitions
-	outRDD=lines.map(lambda x:(map_phase(x),1)).reduceByKey(add).filter(lambda x:x[0].find('tokyo')==-1).sortBy(lambda x:x[1]).collect()
+	outRDD=lines.map(lambda x:(map_phase(x),1)).reduceByKey(add).filter(lambda x:x[0].find('neighborhood')==-1).filter(lambda x:x[0].find('tokyo')==-1).sortBy(lambda x:x[1]).collect()
 	outRDD=sc.parallelize(outRDD)
-	outRDD.saveAsTextFile('output')
+	outRDD.saveAsTextFile('hdfs://localhost:9000/user/hadoop/'+sys.argv[3])
 	
